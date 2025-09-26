e77907a3450a487ebbc3a694b8c98a531325b77f
idan-beren <idanbe7@gmail.com>
2025-09-26 15:17:28 +0300

    Add: adding retry action

--- CliTool/Actions/ActionFactory.cs
+++ CliTool/Actions/ActionFactory.cs
@@ -4,16 +4,19 @@ namespace CliTool.Actions;
 
 public static class ActionFactory
 {
-    public static IAction Create(string name, string type, dynamic config)
+    public static IAction Create(string name, string type, Dictionary<object, object> config)
     {
         return type switch
         {
-            "Log" => new LogAction 
+            "Log" => new LogAction
                 { Name = name, Type = type, Configuration = Reflection.DictionaryToConfig<LogActionConfig>(config) },
             "Parallel" => new ParallelAction
                 { Name = name, Type = type, Configuration = new ParallelActionConfig { Actions = Extractor.GetActions(config) } },
             "Import" => new ImportAction
                 { Name = name, Type = type, Configuration = Reflection.DictionaryToConfig<ImportActionConfig>(config) },
+            "Retry" => new RetryAction
+                { Name = name, Type = type, Configuration = Reflection.DictionaryToConfig<RetryActionConfig>(config)
+                    with { Actions = Extractor.GetActions(config) } },
             _ => throw new Exception($"Unknown action type: {type}")
         };
     }
--- CliTool/Utils/Reflection.cs
+++ CliTool/Utils/Reflection.cs
@@ -11,10 +11,11 @@ public static class Reflection
 
         foreach (var prop in props)
         {
-            if (dict.TryGetValue(prop.Name, out var value))
-            {
-                prop.SetValue(config, Convert.ChangeType(value, prop.PropertyType));
-            }
+            if (!dict.TryGetValue(prop.Name, out var value)) continue;
+            try
+            { prop.SetValue(config, Convert.ChangeType(value, prop.PropertyType)); }
+            catch (Exception)
+            { return config; }
         }
 
         return config;
